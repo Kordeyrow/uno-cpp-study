7ad75b9ba59f190ab096c4300f57eb79cd8a2e0a
Kordeyrow <matheusgoncalvescordeiro@gmail.com>
2024-02-25 21:52:51 -0300

    + Match Setup: Increase, Decrease menu options

--- UnoConsoleGameCpp/include/Uno/Scenes/GameplayScene.h
+++ UnoConsoleGameCpp/include/Uno/Scenes/GameplayScene.h
@@ -6,7 +6,10 @@ public:
     GameplayScene();
     auto Run() -> std::shared_ptr<BaseScene> override;
     auto Init() -> void override;
+    void IncreaseTotalDuelists();
+    void DecreaseTotalDuelists();
 private:
+    int totalDuelists = 2;
     //MatchSetupMenu matchSetupMenu;
 };
 
--- UnoConsoleGameCpp/src/Uno/Scenes/GameplayScene.cpp
+++ UnoConsoleGameCpp/src/Uno/Scenes/GameplayScene.cpp
@@ -9,11 +9,33 @@ auto GameplayScene::Init() -> void
 {
     userInterface->SetTitle("--[ GAMEPLAY ]--");
 
-    //userInterface->AddUserOptions(matchSetupMenu.GetUserActions());
+    userInterface->AddUserOptions({
+
+        std::make_shared<UserOptionData>(
+            "Increase total duelists",
+            std::bind(&GameplayScene::IncreaseTotalDuelists, this)),
+
+        std::make_shared<UserOptionData>(
+            "Decrease total duelists",
+            std::bind(&GameplayScene::DecreaseTotalDuelists, this))
+    });
 }
 
 auto GameplayScene::Run() -> std::shared_ptr<BaseScene>
 {
+    userInterface->SetScene("totalDuelists = " + std::to_string(totalDuelists));
     userInterface->ReadOptionAndExecute();
     return shared_from_this();
+}
+
+void GameplayScene::IncreaseTotalDuelists() {
+    totalDuelists += 1;
+    if (totalDuelists > 12)
+        totalDuelists = 12;
+}
+
+void GameplayScene::DecreaseTotalDuelists() {
+    totalDuelists -= 1;
+    if (totalDuelists < 2)
+        totalDuelists = 2;
 }
