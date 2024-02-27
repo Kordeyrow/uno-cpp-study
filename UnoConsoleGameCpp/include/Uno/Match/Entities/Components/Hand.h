d306cda7190d1535b0f0c8d81b662e95e0b5f030
Kordeyrow <matheusgoncalvescordeiro@gmail.com>
2024-02-27 19:23:12 -0300

    + Card colors const

--- UnoConsoleGameCpp/include/Uno/Match/Entities/Components/Hand.h
+++ UnoConsoleGameCpp/include/Uno/Match/Entities/Components/Hand.h
@@ -3,9 +3,19 @@
 #include <string>
 #include "Card.h"
 
+constexpr auto RED = 31;
+constexpr auto GREEN = 32;
+constexpr auto YELLOW = 33;
+constexpr auto BLUE = 34;
+
 class Hand {
 public:
-    std::vector<Card> deck = { {"7", 31}, {"2", 32} };
+    std::vector<Card> deck = { 
+        {"1", RED},
+        {"2", GREEN},
+        {"3", YELLOW},
+        {"4", BLUE},
+    };
     int getHandSize() const;
     std::string printHand() const;
 };
